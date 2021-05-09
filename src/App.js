fd5b6037fcff242f5efe9b436b5b187de1d2761c
Ridwan Sanusi <rosanusi@gmail.com>
2021-05-09 12:30:53 +0200

    new commits

--- src/App.js
+++ src/App.js
@@ -1,25 +1,76 @@
-import logo from './logo.svg';
-import './App.css';
+
+import {useEffect, useState} from 'react';
+// import styled from "styled-components";
+import './css/main.css';
+
+
+
+// const GridContainer = styled.div`
+//   display: grid;
+//   grid-template-columns: repeat(32, auto);
+//   grid-template-rows: repeat(32, auto);
+//   grid-gap: 1px;
+// `;
 
 function App() {
+
+
+  const [gridArray, setGridArray] = useState([0])
+  const [selectedGrid, setSelectedGrid] = useState(null)
+
+  useEffect(() => {
+    createGridArray()
+  }, [])
+
+  
+
+  let createGridArray = () => {
+    let gridNumber = 1024
+    let gridArray =  Array.from({length: gridNumber}, (v, i) => i);
+
+    let newGrid = [];
+    gridArray.forEach(number => {
+      let blockDetail = {
+        id : number,
+        color : ""
+      }
+      newGrid.push(blockDetail)
+    });
+    setGridArray(newGrid)
+    createBlocks(gridArray)
+  }
+
+  let createBlocks = async(gridArray) => {
+    let container = document.querySelector(".grid-container")
+    await gridArray.forEach(number => {
+      let block = document.createElement("div");
+      block.setAttribute("id", number)
+      block.classList.add("block")
+      container.appendChild(block);  
+    });
+  }
+
+
+  let gridSelected = (e) => {
+    setSelectedGrid(e.target.id)
+  }
+
+
+
+
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container">
+      <div>{selectedGrid}</div>
+      <div className="grid-container" 
+        onClick={gridSelected}
+      >
+
+      </div>
     </div>
   );
+
+  
 }
 
 export default App;
